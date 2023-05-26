7f4408a39872d6f8d16df1b094762dc99649b727
mwk217 <cifidejdj@gmail.com>
2023-05-26 12:18:03 +0900

    LottoPage 패널,라벨 추가 레이아웃 변경

--- LottoProject/src/LottoPage.java
+++ LottoProject/src/LottoPage.java
@@ -1,5 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.LinkedList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,8 +14,8 @@ class LottoTicket extends JFrame {
 	 private LinkedList myNum;
 	    private JButton autoBtn;
 	    private JButton resetBtn;
-	    private JLabel guideLbl;
-	    //private JButton lottoNumBtn;
+	    private JLabel guideLbl1;
+	    private JLabel guideLbl2;
 	    private JButton[] lottoNumBtn;
 	    
 	    public void buttonCreate() {
@@ -25,20 +28,43 @@ class LottoTicket extends JFrame {
 	    
 	    
 	    public LottoTicket() {
+	    	JPanel oneTicketPanel = new JPanel();
+	    	
+	    	JPanel titlePanel = new JPanel();
+	    	JPanel guideLbl1titlePanel = new JPanel();
+	    	JPanel guideLbl2titlePanel = new JPanel();
+	    	JPanel lnbPanel = new JPanel();
+	    	JPanel btnPanel = new JPanel();
+	    	
+	    	
+	    	
+	    	guideLbl1= new JLabel("A");
+	    	guideLbl2 = new JLabel("1000¿ø");
+	    	
+	    	guideLbl1titlePanel.add(guideLbl1);
+	    	guideLbl2titlePanel.add(guideLbl2);
+	    	titlePanel.setLayout(new BoxLayout(titlePanel,BoxLayout.X_AXIS));
+			titlePanel.add(guideLbl1titlePanel);
+			titlePanel.add(guideLbl2titlePanel);
+	    	
+	    	oneTicketPanel.setLayout(new BoxLayout(oneTicketPanel, BoxLayout.Y_AXIS));
+	    	
 	    	
 	    	lottoNumBtn = new JButton[45];
 	    	
 	    	
 	    	buttonCreate();
 	    	
-	    	
-	    	
-	    	JPanel lnbPanel = new JPanel();
+	    	lnbPanel.setLayout(new GridLayout(6,8));
 	    	for(int i=0;i<lottoNumBtn.length;i++) {
 	    		lnbPanel.add(lottoNumBtn[i]);
 	    	}
 	    	
-	    	add(lnbPanel);
+	    	add(oneTicketPanel);
+	    	oneTicketPanel.add(titlePanel);
+	    	oneTicketPanel.add(lnbPanel);
+	    	oneTicketPanel.add(btnPanel);
+	    	
 	    	setSize(600, 1000);
 	    	setVisible(true);
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
