33cefc9ccea9d54eae7687f478f1f941d7105044
Psh230412 <sanghyeon0402@gmail.com>
2023-06-01 17:15:37 +0900

    unNumber 커밋

--- LottoProject/src/Management.java
+++ LottoProject/src/Management.java
@@ -1,4 +1,5 @@
 import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.LayoutManager;
 
@@ -16,16 +17,14 @@ public class Management extends JFrame {
 		all.add(new StartPage(), "시작");
 		all.add(new LottoNumberPage(), "번호 선택");
 		all.add(new LottoTicket1(), "티켓1");
-		all.add(new LottoTicket2(), "티켓2");
-		all.add(new LottoTicket3(), "티켓3");
-		all.add(new LottoTicket4(), "티켓4");
-		all.add(new LottoTicket5(), "티켓5");
 //		all.add(new LottoDrawPage(), "당첨 번호");
 
-//		card.show(all, "시작");
+
+		card.show(all, "공");
 		add(all);
 		
-		setSize(340, 594);
+		all.setPreferredSize(new Dimension(340,550));
+		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		
