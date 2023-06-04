01da6c7a2ad88889887059c9adedb63486d16ed3
honghong77 <mihong812@gmail.com>
2023-06-04 23:00:13 +0900

    드로우 페이지 위치 수정

--- LottoProject/src/LottoDrawPage.java
+++ LottoProject/src/LottoDrawPage.java
@@ -29,9 +29,8 @@ public class LottoDrawPage extends JPanel {
 	JLabel[] numArrD = makeNumLbl();
 	JLabel[] numArrE = makeNumLbl();
 	JLabel[] drawNumArr = makeDrawNumLbl();
-	JLabel[] screenArr = new JLabel[16];
+	JLabel[] screenArr = new JLabel[14];
 	Management management;
-	
 	
 	// index 6 : 자동, 7 : 등수
 	public JLabel[] makeNumLbl() {
@@ -50,7 +49,7 @@ public class LottoDrawPage extends JPanel {
 		return drawNumArr;
 	}
 	
-	public JLabel[] createScreen() {
+	public JLabel[] makeScreen() {
 		for (int i = 0; i < screenArr.length; i++) {
 			URL screen = LottoDrawPage.class.getClassLoader().getResource("image4/결과확인" + (i + 1) + ".gif");
 			ImageIcon imageIcon = new ImageIcon(screen);
@@ -64,79 +63,83 @@ public class LottoDrawPage extends JPanel {
 		for (int i = 0; i < numArrA.length; i++) {
 			add(numArrA[i]);
 		}
-		numArrA[0].setBounds(0, 285, 43, 42);
-		numArrA[1].setBounds(43, 285, 42, 42);
-		numArrA[2].setBounds(43+42, 285, 42, 42);
-		numArrA[3].setBounds(43+42+42 , 285, 42, 42);
-		numArrA[4].setBounds(43+42+42+42, 285, 42, 42);
-		numArrA[5].setBounds(43+42+42+42+42, 285, 42, 42);
-		numArrA[6].setBounds(43+42+42+42+42+42, 285, 42, 42);
-		numArrA[7].setBounds(43+42+42+42+42+42+42, 285, 45, 42);
+		numArrA[0].setBounds(0, 200, 43, 42);
+		numArrA[1].setBounds(43, 200, 42, 42);
+		numArrA[2].setBounds(43+42, 200, 42, 42);
+		numArrA[3].setBounds(43+42+42 , 200, 42, 42);
+		numArrA[4].setBounds(43+42+42+42, 200, 42, 42);
+		numArrA[5].setBounds(43+42+42+42+42, 200, 42, 42);
+		numArrA[6].setBounds(43+42+42+42+42+42, 200, 42, 42);
+		numArrA[7].setBounds(43+42+42+42+42+42+42, 200, 45, 42);
 		for (int i = 0; i < numArrB.length; i++) {
 			add(numArrB[i]);
 		}
-		numArrB[0].setBounds(0, 285+42, 43, 42);
-		numArrB[1].setBounds(43, 285+42, 42, 42);
-		numArrB[2].setBounds(43+42, 285+42, 42, 42);
-		numArrB[3].setBounds(43+42+42 , 285+42, 42, 42);
-		numArrB[4].setBounds(43+42+42+42, 285+42, 42, 42);
-		numArrB[5].setBounds(43+42+42+42+42, 285+42, 42, 42);
-		numArrB[6].setBounds(43+42+42+42+42+42, 285+42, 42, 42);
-		numArrB[7].setBounds(43+42+42+42+42+42+42, 285+42, 45, 42);
+		numArrB[0].setBounds(0, 200+67, 43, 42);
+		numArrB[1].setBounds(43, 200+67, 42, 42);
+		numArrB[2].setBounds(43+42, 200+67, 42, 42);
+		numArrB[3].setBounds(43+42+42 , 200+67, 42, 42);
+		numArrB[4].setBounds(43+42+42+42, 200+67, 42, 42);
+		numArrB[5].setBounds(43+42+42+42+42, 200+67, 42, 42);
+		numArrB[6].setBounds(43+42+42+42+42+42, 200+67, 42, 42);
+		numArrB[7].setBounds(43+42+42+42+42+42+42, 200+67, 45, 42);
 		for (int i = 0; i < numArrC.length; i++) {
 			add(numArrC[i]);
 		}
-		numArrC[0].setBounds(0, 285+42+42, 43, 42);
-		numArrC[1].setBounds(43, 285+42+42, 42, 42);
-		numArrC[2].setBounds(43+42, 285+42+42, 42, 42);
-		numArrC[3].setBounds(43+42+42 , 285+42+42, 42, 42);
-		numArrC[4].setBounds(43+42+42+42, 285+42+42, 42, 42);
-		numArrC[5].setBounds(43+42+42+42+42, 285+42+42, 42, 42);
-		numArrC[6].setBounds(43+42+42+42+42+42, 285+42+42, 42, 42);
-		numArrC[7].setBounds(43+42+42+42+42+42+42, 285+42+42, 45, 42);
+		numArrC[0].setBounds(0, 200+67+67, 43, 42);
+		numArrC[1].setBounds(43, 200+67+67, 42, 42);
+		numArrC[2].setBounds(43+42, 200+67+67, 42, 42);
+		numArrC[3].setBounds(43+42+42 , 200+67+67, 42, 42);
+		numArrC[4].setBounds(43+42+42+42, 200+67+67, 42, 42);
+		numArrC[5].setBounds(43+42+42+42+42, 200+67+67, 42, 42);
+		numArrC[6].setBounds(43+42+42+42+42+42, 200+67+67, 42, 42);
+		numArrC[7].setBounds(43+42+42+42+42+42+42, 200+67+67, 45, 42);
 		for (int i = 0; i < numArrD.length; i++) {
 			add(numArrD[i]);
 		}
-		numArrD[0].setBounds(0, 285+42+42+42, 43, 42);
-		numArrD[1].setBounds(43, 285+42+42+42, 42, 42);
-		numArrD[2].setBounds(43+42, 285+42+42+42, 42, 42);
-		numArrD[3].setBounds(43+42+42 , 285+42+42+42, 42, 42);
-		numArrD[4].setBounds(43+42+42+42, 285+42+42+42, 42, 42);
-		numArrD[5].setBounds(43+42+42+42+42, 285+42+42+42, 42, 42);
-		numArrD[6].setBounds(43+42+42+42+42+42, 285+42+42+42, 42, 42);
-		numArrD[7].setBounds(43+42+42+42+42+42+42, 285+42+42+42, 45, 42);
+		numArrD[0].setBounds(0, 200+67+67+67, 43, 42);
+		numArrD[1].setBounds(43, 200+67+67+67, 42, 42);
+		numArrD[2].setBounds(43+42, 200+67+67+67, 42, 42);
+		numArrD[3].setBounds(43+42+42 , 200+67+67+67, 42, 42);
+		numArrD[4].setBounds(43+42+42+42, 200+67+67+67, 42, 42);
+		numArrD[5].setBounds(43+42+42+42+42, 200+67+67+67, 42, 42);
+		numArrD[6].setBounds(43+42+42+42+42+42, 200+67+67+67, 42, 42);
+		numArrD[7].setBounds(43+42+42+42+42+42+42, 200+67+67+67, 45, 42);
 		for (int i = 0; i < numArrE.length; i++) {
 			add(numArrE[i]);
 		}
-		numArrE[0].setBounds(0, 285+42+42+42+42, 43, 42);
-		numArrE[1].setBounds(43, 285+42+42+42+42, 42, 42);
-		numArrE[2].setBounds(43+42, 285+42+42+42+42, 42, 42);
-		numArrE[3].setBounds(43+42+42 , 285+42+42+42+42, 42, 42);
-		numArrE[4].setBounds(43+42+42+42, 285+42+42+42+42, 42, 42);
-		numArrE[5].setBounds(43+42+42+42+42, 285+42+42+42+42, 42, 42);
-		numArrE[6].setBounds(43+42+42+42+42+42, 285+42+42+42+42, 42, 42);
-		numArrE[7].setBounds(43+42+42+42+42+42+42, 285+42+42+42+42, 45, 42);
+		numArrE[0].setBounds(0, 200+67+67+67+67, 43, 42);
+		numArrE[1].setBounds(43, 200+67+67+67+67, 42, 42);
+		numArrE[2].setBounds(43+42, 200+67+67+67+67, 42, 42);
+		numArrE[3].setBounds(43+42+42 , 200+67+67+67+67, 42, 42);
+		numArrE[4].setBounds(43+42+42+42, 200+67+67+67+67, 42, 42);
+		numArrE[5].setBounds(43+42+42+42+42, 200+67+67+67+67, 42, 42);
+		numArrE[6].setBounds(43+42+42+42+42+42, 200+67+67+67+67, 42, 42);
+		numArrE[7].setBounds(43+42+42+42+42+42+42, 200+67+67+67+67, 45, 42);
 
 		// 당첨 번호 라벨
 		for (int i = 0; i < drawNumArr.length; i++) {
 			add(drawNumArr[i]);
 		}
-		drawNumArr[0].setBounds(15, 216, 42, 42);
-		drawNumArr[1].setBounds(15+(42*1), 216, 42, 42);
-		drawNumArr[2].setBounds(15+(42*2), 216, 42, 42);
-		drawNumArr[3].setBounds(15+(42*3), 216, 42, 42);
-		drawNumArr[4].setBounds(15+(42*4), 216, 42, 42);
-		drawNumArr[5].setBounds(15+(42*5), 216, 42, 42);
-		drawNumArr[6].setBounds(15+(42*6)+17, 216, 42, 42);
+		drawNumArr[0].setBounds(15, 133, 42, 42);
+		drawNumArr[1].setBounds(15+(42*1), 133, 42, 42);
+		drawNumArr[2].setBounds(15+(42*2), 133, 42, 42);
+		drawNumArr[3].setBounds(15+(42*3), 133, 42, 42);
+		drawNumArr[4].setBounds(15+(42*4), 133, 42, 42);
+		drawNumArr[5].setBounds(15+(42*5), 133, 42, 42);
+		drawNumArr[6].setBounds(15+(42*6)+17, 133, 42, 42);
 
 		// 기본 배경
-		JLabel[] lbl = createScreen();
-		lbl[0].setBounds(0, 0, 340, 189);
-		lbl[1].setBounds(0, 189, 284, 27);
-		lbl[4].setBounds(284, 189 + 15, 42, 12); // 보너스
-		lbl[6].setBounds(15 + 252, 189 + 15 + 12, 17, 42); // 더하기
-		lbl[12].setBounds(23 + 15 + 265, 189 + 27 + 42 + 27 + 210 + 20, 15, 18); // 다음 버튼
-
+		JLabel[] lbl = makeScreen();
+		lbl[0].setBounds(0, 0, 340, 121);
+		lbl[2].setBounds(284, 121, 42, 12); // 보너스
+		lbl[5].setBounds(15 + 252, 121 + 12, 17, 42); // 더하기
+		lbl[6].setBounds(0, 175, 340, 25); // A
+		lbl[7].setBounds(0, 121 + 12 + 42 + 67, 340, 25); // B
+		lbl[8].setBounds(0, 121 + 12 + 42 + 67 + 67, 340, 25); // C
+		lbl[9].setBounds(0, 121 + 12 + 42 + 67 + 67 + 67, 340, 25); // D
+		lbl[10].setBounds(0, 121 + 12 + 42 + 67 + 67 + 67 + 67, 340, 25); // E
+		lbl[12].setBounds(118, 121 + 12 + 42 + 67 + 67 + 67 + 67 + 67, 100, 40); // 메인
+		
 		for (int i = 0; i < lbl.length; i++) {
 			add(lbl[i]);
 		}
